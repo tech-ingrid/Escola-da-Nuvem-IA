944d67a86507733f48914b999656060a83ba0868
Ingrid Santos <68664992+tech-ingrid@users.noreply.github.com>
2025-03-27 23:03:46 -0300

    Add files via upload

---
+++ aula03.py
@@ -0,0 +1,23 @@
+#Crie um programa para verificar a maioridade
+
+# Solicita a idade do usuário
+idade = int(input("Digite a sua idade: "))
+
+# Verifica se é maior de idade e exibe a mensagem adequada
+if idade >= 18:
+    print("Você é maior de idade.")
+elif idade >= 12:
+    print("Você é um Adolescente")
+else:
+#Crie um programa para verificar a maioridade
+
+# Solicita a idade do usuário
+    idade = int(input("Digite a sua idade: "))
+
+# Verifica se é maior de idade e exibe a mensagem adequada
+if  idade >= 18:
+    print("Você é maior de idade.")
+elif idade >= 12:
+    print("Você é um Adolescente")
+else:
+    print("Você é menor de idade.")
