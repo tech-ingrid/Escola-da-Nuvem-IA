bc3444cc726c3bcc051c9311faa29e26886c7394
Ingrid Santos <68664992+tech-ingrid@users.noreply.github.com>
2025-03-28 20:22:52 -0300

    Aula12

---
+++ aula12.py
@@ -0,0 +1,24 @@
+import requests
+
+def obter_usuario_aleatorio():
+	url = "https://randomuser.me/api/"
+	try:
+		response = requests.get("https://randomuser.me/api/")
+		response.raise_for_status
+		dados = response.json()['results'][0]
+		nome = f"{dados['name']['first']} {dados['name']['last']}"
+		email = dados['email']
+		pais = dados['location']['country']
+		return f"Nome:{nome}\nEmail:{email}\Pais: {pais}"
+	except requests.RequestException as e:
+		return f"Erro ao obter usuário aleatório:{e}"
+
+def main():
+	print("Gerando um usuário aleatório...")
+	usuario = obter_usuario_aleatorio()
+	print(usuario)
+	
+if __name__== "__main__":
+	main()
+	
+	
